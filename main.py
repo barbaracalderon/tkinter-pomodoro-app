4ab2450103254bf9926c571283e99dedb0f00ac9
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-10-21 21:08:09 -0300

    Adjusted the window layout: colors, font, position

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 import tkinter
-# ---------------------------- CONSTANTS ------------------------------- #
+
+# ----- CONSTANTS ----- #
 PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Verdana"
+RED = "#C84B31"
+GREEN = "#346751"
+YELLOW = "#ECDBBA"
+FONT_NAME = "Lucida Console"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
-# ---------------------------- UI SETUP ------------------------------- #
+# ----- UI SETUP ----- #
 window = tkinter.Tk()
 window.title("The Pomodoro App")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=50, pady=50, bg=RED)
 
-canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = tkinter.PhotoImage(file="tomato.png")
+# --- UI SETUP: Components --- #
+
+title_label = tkinter.Label(text="POMODORO", font=(FONT_NAME, 30), bg=RED, fg=YELLOW)
+canvas = tkinter.Canvas(width=200, height=224, bg=RED, highlightthickness=0)
+
+tomato_img = tkinter.PhotoImage(file="tomato_a.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35))
 
-canvas.pack()
+start_button = tkinter.Button(text="Start", font=(FONT_NAME, 10), highlightthickness=0)
+checkmark_label = tkinter.Label(text="✓", font=(FONT_NAME, 35, "bold"), bg=RED, fg=GREEN)
+reset_button = tkinter.Button(text="Reset", font=(FONT_NAME, 10), highlightthickness=0)
 
+
+# --- UI SETUP: Grid Layout --- #
+title_label.grid(column=1, row=0)
+canvas.grid(column=1, row=1)
+
+start_button.grid(column=0, row=2)
+checkmark_label.grid(column=1, row=3)
+reset_button.grid(column=2, row=2)
+
+# --- Window Loop --- #
 window.mainloop()
 

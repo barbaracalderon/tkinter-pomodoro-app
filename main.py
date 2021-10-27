7cef3f211994aa7d79f2de58498d2e50c62a9dc4
Barbara Calderon <69126514+barbaracalderon@users.noreply.github.com>
2021-10-27 16:29:51 -0300

    More size to long break tittle

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def start_timer():
     long_break_sec = LONG_BREAK_MIN * 60
 
     if repetition % 8 == 0:
-        title_task_label.config(text="long break", font=(FONT_NAME, 20), bg=GREEN, fg=GREEN_MSG)
+        title_task_label.config(text="long break", font=(FONT_NAME, 30), bg=GREEN, fg=GREEN_MSG)
         start_countdown(long_break_sec)
     elif repetition % 2 == 0:
         start_countdown(short_break_sec)
